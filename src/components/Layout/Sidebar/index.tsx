de3e9a66d9bc16a95ea39415f0f14b190ed7158f
Nikolay Lanets <n.lanets@freecode.academy>
2026-08-16 22:02:29 +0300

    Hide unused menu items

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -77,6 +77,7 @@ const navItems: Array<NavItem | undefined> = [
         <path d="M16 3.13a4 4 0 0 1 0 7.75" />
       </svg>
     ),
+    accessPolicy: 'sudo',
   },
   {
     label: 'Tasks',
@@ -94,6 +95,7 @@ const navItems: Array<NavItem | undefined> = [
         <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11" />
       </svg>
     ),
+    accessPolicy: 'sudo',
   },
   {
     label: 'Admin',
